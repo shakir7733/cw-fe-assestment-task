91aab384da7e9e695596386db0cb24122bdd85c9
Shakir Khan <samshake7@gmail.com>
2025-07-19 16:43:56 +0545

    updated design and added proper tag for a semantic structure

--- src/components/feature/taglist.tsx
+++ src/components/feature/taglist.tsx
@@ -8,13 +8,13 @@ interface TagListProps {
 export const TagList = (props: TagListProps) => {
     const { title, tags } = props;
     return (
-        <div className="mt-8 px-6 max-w-5xl mx-auto">
-            <div className="text-white text-lg font-semibold mb-4">{title}</div>
-            <div className="flex flex-wrap gap-3">
+        <div className="mx-auto">
+            <h2 className="text-white px-4 pt-4 text-lg font-semibold">{title}</h2> {/* Use h2 for semantic structure */}
+            <div className="flex flex-wrap gap-3 px-3 py-3">
                 {tags.map((tag) => (
                     <Badge
-                        key={tag}
-                        className="bg-gray-800 text-white hover:bg-gray-700 cursor-pointer"
+                        key={tag} // Assuming tags are unique strings, which they appear to be here.
+                        className="bg-muted text-muted-foreground rounded-lg  h-8 px-4 text-sm font-medium hover:bg-gray-700 cursor-pointer"
                     >
                         {tag}
                     </Badge>
