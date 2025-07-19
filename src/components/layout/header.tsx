4adcf87f19bc7e29afd9bf49ff82d9dc659c6bf7
Shakir Khan <samshake7@gmail.com>
2025-07-19 17:10:27 +0545

    created usedebounce hook and added inside header search

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,21 +3,37 @@ import { Search } from "lucide-react"
 import { Input } from "../ui/input"
 import logo from "@/assets/images/logo.png"
 import avatarImage from "@/assets/images/avatar.png"
+import { useDebounce } from "@/hooks/use-debounce"
+import { useEffect, useState } from "react"
+
+const onSearch = (term: string) => {
+    console.log("Searching for:", term);
+    // Implement search logic here
+};
 
 export const Header = () => {
+    const [searchTerm, setSearchTerm] = useState<string>("")
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+    useEffect(() => {
+        if (debouncedSearchTerm) { // Only search if there's a value
+            onSearch(debouncedSearchTerm);
+        }
+    }, [debouncedSearchTerm]);
+
     return (
         <header className="flex items-center justify-between px-10 py-4 bg-background border-b border-muted-foreground">
             <div className="flex items-center gap-4">
                 <img src={logo} alt="Logo" className="w-4 h-4" />
                 <div className="text-white font-semibold text-lg">Wortionary</div>
             </div>
-
             <div className="flex items-center gap-8">
                 <div className="bg-muted relative flex items-center w-40 h-10 rounded-lg pl-4 pr-1">
                     <Search className="text-muted-foreground text-sm" />
                     <Input
                         type="text"
                         placeholder="Search"
+                        onChange={(e) => setSearchTerm(e.target.value)}
                         className="text-muted-foreground border-none focus-visible:ring-0 focus-visible:ring-offset-0 rounded-full h-9 placeholder:text-muted-foreground placeholder:text-[16px]"
                     />
                 </div>
